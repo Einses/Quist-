50c4805cce3d5bf884a8d6072ca0c8b3aa4de1ea
Sangrio, Epiphany Jim <117999186+Einses@users.noreply.github.com>
2022-12-10 21:49:51 +0800

    newstart

---
+++ quist/quiz/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ex: /polls/
+    path('frontend', views.frontend, name='frontend'),
+    path('gameloading', views.gameloading, name='gameloading'),
+]
--- quist/quiz/views.py
+++ quist/quiz/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def frontend(request):
     return render(request, "frontend.html")
+
+def gameloading(request):
+    return render(request, "gameloading.html")
