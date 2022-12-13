3e7ef1b2c5d10fbf905afcf55e7fa6b56959d435
Yet Joshua <95950220+SynthSix@users.noreply.github.com>
2022-12-13 23:18:53 +0800

    Models Updates & Admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-from .models import Question, Choice
+from .models import Question, Answer
 
-# Register your models here.
-@admin.register(Question)
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text',)
+    inlines = [AnswerInline]
 
-@admin.register(Choice)
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('question', 'ans1','ans2','ans3')
-
-class Q_and_A(admin.AdminSite):
-    site_header = 'Quist Admin area'
-
-QA_site = Q_and_A(name='BlogAdmin')
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
