3c0b3651ea7112cf1e3d78c02f5d543727dbed40
Sangrio, Epiphany Jim <112065237+GarfinKarhl@users.noreply.github.com>
2022-12-14 13:54:03 +0800

    buttonanimation

--- quiz/models.py
+++ quiz/models.py
@@ -6,19 +6,17 @@ class Question(models.Model):
     #quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def str(self):
         return str(self.text)
 
     def get_answers(self):
         return self.answer_set.all()
 
 class Answer(models.Model):
-    ans1 = models.CharField(max_length=200)
-    ans2 = models.CharField(max_length=200)
-    ans3 = models.CharField(max_length=200)
-    correct = models.BooleanField(default=False)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    ans1 = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def str(self):
         return f"question: {self.question.text}, answer: {self.text}, correct: {self.correct}"
